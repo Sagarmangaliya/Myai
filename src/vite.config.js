6e4ef54ec9e0211edf14bf919a55a361504301d1
Sagarmangaliya <76936702+Sagarmangaliya@users.noreply.github.com>
2025-02-10 01:04:11 +0530

    Delete src/vite.config.js

--- src/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    allowedHosts: [
-      "fe118761-bd62-46a8-8b9e-5174d9779133-00-3444qikyspnsi.sisko.replit.dev"
-    ]
-  }
-});

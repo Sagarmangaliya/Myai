7ba392be806c649d355ddc132c72d65a63c10139
Sagarmangaliya <76936702+Sagarmangaliya@users.noreply.github.com>
2025-02-09 23:08:09 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>AI Video Processing App</h1>
+      <p>Upload your video and process it using AI!</p>
+    </div>
+  );
+}
+
+export default App;

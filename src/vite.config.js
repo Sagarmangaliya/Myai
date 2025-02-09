1c0dc357a9c936207ea4fb54bde5ff7944717e57
Sagarmangaliya <76936702+Sagarmangaliya@users.noreply.github.com>
2025-02-09 23:43:06 +0530

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   server: {
     port: 5173,
-  },
+    host: "0.0.0.0", // External access allow करने के लिए
+    strictPort: true, 
+    allowedHosts: [
+      "fe118761-bd62-46a8-8b9e-5174d9779133-00-3444qikyspnsi.sisko.replit.dev", // Replit का होस्ट जोड़ें
+      "localhost"
+    ]
+  }
 });

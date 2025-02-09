bdb5ca0fa3ee0423c832b532b93c11e1475b2660
Sagarmangaliya <76936702+Sagarmangaliya@users.noreply.github.com>
2025-02-10 00:32:30 +0530

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from "vite";
-
 export default defineConfig({
   server: {
-    port: 5173,
-    host: "0.0.0.0", // सभी नेटवर्क से एक्सेस की अनुमति  
-    strictPort: true,
-    cors: true,
     allowedHosts: [
-      "localhost",
       "fe118761-bd62-46a8-8b9e-5174d9779133-00-3444qikyspnsi.sisko.replit.dev"
     ]
   }
